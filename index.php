e4cb744bf2859ba41f97edceee158deceb8996cf
Guerra2305 <99186842+Guerra2305@users.noreply.github.com>
2023-03-02 09:54:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo"ola mundo"?></h1>
+    <h2>9+7=<?php echo (9+7)?></h2>
+    <h2>10-7=<?php echo (10-7)?></h2>
+    <h2>2*8=<?php echo (2*8)?></h2>
+    <h2>16/4=<?php echo (16/4)?></h2>
+
+</body>
+</html>
